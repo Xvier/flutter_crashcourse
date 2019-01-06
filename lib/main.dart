bc84fe18bf230006af9a4541fc72c5e5edd4c2f4
Xavier <xviuda@xebia.com>
2019-01-06 21:41:31 +0100

    removed imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'location_detail.dart';
 import 'location_list.dart';
 import 'mocks/mock_location.dart';
-import 'models/location.dart';
 
 void main() {
   final mockLocations = MockLocation.fetchAll();
